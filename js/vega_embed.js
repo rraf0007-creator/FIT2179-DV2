e21245fb52a2ea0dd3e66f1f236fd2241a6c86b1
Rafi <ridw.rafi@gmail.com>
2026-05-18 18:28:26 +1000

    Create initial webpage layout

---
+++ js/vega_embed.js
@@ -0,0 +1,6 @@
+vegaEmbed("#top-countries-bar", "visualisations/top_countries_bar.vg.json").catch(console.error);
+
+vegaEmbed("#yearly-trend-line", "visualisations/yearly_trend_line.vg.json").catch(console.error);
+
+// We will add this after creating the map spec.
+// vegaEmbed("#world-map", "visualisations/world_map.vg.json").catch(console.error);
