fbbf362d9b67bfaa2bc88d651a49412144f87453
Rafi <ridw.rafi@gmail.com>
2026-05-18 20:05:57 +1000

    Add state and sector visualisations

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -2,5 +2,9 @@ vegaEmbed("#top-countries-bar", "visualisations/top_countries_bar.vg.json").catc
 
 vegaEmbed("#yearly-trend-line", "visualisations/yearly_trend_line.vg.json").catch(console.error);
 
+vegaEmbed("#state-bar", "visualisations/state_bar.vg.json").catch(console.error);
+
+vegaEmbed("#sector-breakdown", "visualisations/sector_breakdown.vg.json").catch(console.error);
+
 // We will add this after creating the map spec.
 // vegaEmbed("#world-map", "visualisations/world_map.vg.json").catch(console.error);
