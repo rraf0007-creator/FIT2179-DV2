1e4d230b1cb992995e7962ff8ef8b456a0303104
Rafi <ridw.rafi@gmail.com>
2026-05-18 21:52:59 +1000

    Added more charts

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -7,3 +7,11 @@ vegaEmbed("#state-bar", "visualisations/state_bar.vg.json").catch(console.error)
 vegaEmbed("#sector-breakdown", "visualisations/sector_breakdown.vg.json").catch(console.error);
 
 vegaEmbed("#australia-bubble-map", "visualisations/australia_bubble_map.vg.json").catch(console.error);
+
+vegaEmbed("#abs-student-migration-trend", "visualisations/abs_student_migration_trend.vg.json").catch(console.error);
+
+vegaEmbed("#combined-state-migration-enrolments", "visualisations/combined_state_migration_enrolments_bubble.vg.json").catch(console.error);
+
+vegaEmbed("#abs-temp-visa-lollipop", "visualisations/abs_temp_visa_lollipop.vg.json").catch(console.error);
+
+vegaEmbed("#abs-state-net-heatmap", "visualisations/abs_state_net_heatmap.vg.json").catch(console.error);
